3d5da8b410bbb843ad3b012b1498c6385ca1b88d
mhanley00 <mehanley2@gmail.com>
2018-08-21 07:49:33 -0400

    added psudo-code for functionality, reorganized files as suggested

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+var khaledKeys = ["they", "blessed", "all the keys", "water your plants", "lion order", "live smooth", "all i do is win", "we the best"];
+var userBands = prompt("Do you have all the keys?").toLowerCase();
+
+if (favoriteBands.indexOf(userBands) == -1) {
+    alert("Nah. They're pretty lame.");
+}
+else {
+    alert("YEAH I LOVE THEM!");
+}
+
+// for reference:
+//     var likeSteak = confirm("Do you like steak?");
+//     var ofAge = prompt("How old are you?");
+
+//     if (likeSteak) {
+//         alert("Here's a steak sandwich");
+//     }
+
+//     else {
+//         alert("Here's a Tofu Stir-Fry");
+//     }
+
+//     if (ofAge >= 21) {
+//         alert("Kampai");
+//     }
+
+//     else {
+//         alert("No Sake for you!");
+
+//     }
