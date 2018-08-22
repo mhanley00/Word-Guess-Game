e0a444dc735186f7290bd2b255f408fd2034a810
mhanley00 <mehanley2@gmail.com>
2018-08-22 07:03:09 -0400

    added some code from rock/paper/scissors example to experiment

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,23 @@
 var khaledKeys = ["they", "blessed", "all the keys", "water your plants", "lion order", "live smooth", "all i do is win", "we the best"];
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var userBands = prompt("Do you have all the keys?").toLowerCase();
+var guesses = 10;
+var userGuesses = 0;
 
-if (favoriteBands.indexOf(userBands) == -1) {
-    alert("Nah. They're pretty lame.");
-}
-else {
-    alert("YEAH I LOVE THEM!");
-}
+// This function is run whenever the user presses a key.
+document.onkeyup = function (event) {
+// Determines which key was pressed.
+    var userGuess = event.key;
+
+
+
+
+//     if (favoriteBands.indexOf(userBands) == -1) {
+//     alert("Nah. They're pretty lame.");
+// }
+// else {
+//     alert("YEAH I LOVE THEM!");
+// }
 
 // for reference:
 //     var likeSteak = confirm("Do you like steak?");
